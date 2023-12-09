38c06efe477920a263f3c0024680370ad32e89fb
abdulahi roble <abdulahi_2610@hotmail.com>
2023-12-09 18:19:35 +0100

    Refactor Navbar and AssetQualityTable components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,43 +10,36 @@ const items: MenuProps['items'] = [
         icon: <MailOutlined />,
     },
     {
-        label: 'Navigation Two',
-        key: 'app',
-        icon: <AppstoreOutlined />,
-        disabled: true,
-    },
-    {
-        label: 'Navigation Three - Submenu',
-        key: 'SubMenu',
+        label: 'Menu',
+        key: 'Menu',
         icon: <SettingOutlined />,
         children: [
             {
                 type: 'group',
-                label: 'Item 1',
-                children: [
-                    {
-                        label: 'Option 1',
-                        key: 'setting:1',
-                    },
-                    {
-                        label: 'Option 2',
-                        key: 'setting:2',
-                    },
-                ],
+                label: (
+                    <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+                        Change tenant
+                    </a>
+                ),
+                key: 'change-tenant',
             },
             {
                 type: 'group',
-                label: 'Item 2',
-                children: [
-                    {
-                        label: 'Option 3',
-                        key: 'setting:3',
-                    },
-                    {
-                        label: 'Option 4',
-                        key: 'setting:4',
-                    },
-                ],
+                label: (
+                    <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+                        Account settings
+                    </a>
+                ),
+                key: 'account-settings',
+            },
+            {
+                type: 'group',
+                label: (
+                    <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+                        Logout
+                    </a>
+                ),
+                key: 'logout',
             },
         ],
     },
