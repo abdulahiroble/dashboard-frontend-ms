9aa53f595737696df69ed9c3f1d9ef7b33a407c1
abdulahi roble <abdulahi_2610@hotmail.com>
2023-12-25 14:24:11 +0100

    Add LoadflowCollection to ModalComponent

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -5,6 +5,7 @@ import { MarkerF } from '@react-google-maps/api';
 import Logo from '../logo.svg';
 import LoadFerretCountsCollection from '../services/collections/LoadFerretCountsCollection';
 import LoadStationsCollection from '../services/collections/LoadStationsCollection';
+import LoadflowCollection from '../services/collections/LoadflowCollection';
 
 const ModalComponent = ({ stations }: any) => {
     console.log(stations)
@@ -13,15 +14,22 @@ const ModalComponent = ({ stations }: any) => {
     const [selectedStation, setSelectedStation] = useState(null);
     const [ferretCounts, setFerretCounts] = useState(null);
     const [station, setStation] = useState(null);
+    const [loadflow, setLoadflow] = useState<any>(null);
 
-    const showModal = () => {
+    const showModal = (station: any) => {
         setIsModalOpen(true);
+        // console.log(station)
         setSelectedStation(station)
 
         LoadFerretCountsCollection.getAllFerrets().then((result: any) => {
             setFerretCounts(result.object)
         })
+        LoadflowCollection.getAllStations().then((result: any) => {
+            setLoadflow(result.object)
+        })
     };
+
+    // console.log(loadflow.map((item: any) => item.primary_substation))
 
 
     const handleOk = () => {
@@ -32,22 +40,44 @@ const ModalComponent = ({ stations }: any) => {
         setIsModalOpen(false);
     };
 
-    const stationCity = (station: any) => {
-        console.log(station)
-    }
-
     return (
         <>
             <div>
                 {stations && (
                     stations.map((marker: any) => (
-                        <MarkerF key={marker.id} position={marker.position} onClick={() => showModal()} />
+                        <MarkerF key={marker.id} position={marker.position}
+                            onClick={() => showModal(marker.id)}
+                        />
                     ))
                 )}
-                <Modal title={"Basic Modal"} open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-                    <p>Some contents...</p>
-                    <p>Some contents...</p>
-                    <p>Some contents...</p>
+                <Modal title={`Station - ${selectedStation}`} open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                    {/* {loadflow?.map((item: any) => item.primary_substation === "HOL" && (
+                        <div>
+                            <h3>Loadflow</h3>
+                            <p>Name: {item.name}</p>
+                            <p>Substation: {item.primary_substation}</p>
+                            <p>Period: {item.period}</p>
+                            <p>Time stamps: {item.n_timestamps}</p>
+                            <p>Failed: {item.n_failed}</p>
+                            <p>Success: {item.success_percentage}</p>
+                            <p>Simulation: {item.simulation_id}</p>
+                            <p>Version: {item.version_id}</p>
+                        </div>
+                    ))} */}
+                    {loadflow?.slice(0, 4).map((item: any) => item.primary_substation === selectedStation && (
+                        <div key={item.id}>
+                            <h3>Loadflow</h3>
+                            <p>Name: {item.name}</p>
+                            <p>Substation: {item.primary_substation}</p>
+                            <p>Period: {item.period}</p>
+                            <p>Time stamps: {item.n_timestamps}</p>
+                            <p>Failed: {item.n_failed}</p>
+                            <p>Success: {item.success_percentage}</p>
+                            <p>Simulation: {item.simulation_id}</p>
+                            <p>Version: {item.version_id}</p>
+                        </div>
+                    ))}
+
                 </Modal>
             </div>
         </>
---
+++ src/services/collections/LoadflowCollection.tsx
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+
+// let url = process.env.REACT_APP_DEVELOPMENT_SEARCH_SERVICE_URL;
+// if (process.env.REACT_APP_ENVIRONMENT != 'prod') {
+//     url = process.env.REACT_APP_DEVELOPMENT_URL;
+// }
+class LoadflowCollection {
+    getAllStations = async () => {
+        const result = await axios.get(`http://localhost:3004/api/get/loadflow-nkforsyning`);
+
+        return result.data
+    }
+}
+
+export default new LoadflowCollection();
