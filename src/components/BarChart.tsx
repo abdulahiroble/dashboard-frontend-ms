fae196c986dafa4fa57ecd9f2f7914314160fd5a
abdulahi roble <abdulahi_2610@hotmail.com>
2023-12-28 13:50:43 +0100

    Show dates in x axis

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -3,6 +3,11 @@ import React, { useEffect, useMemo, useState } from 'react'
 import { HighchartsReact } from 'highcharts-react-official';
 import Highcharts from 'highcharts'
 import LoadflowCollection from '../services/collections/LoadflowCollection';
+import HighchartsExporting from 'highcharts/modules/exporting';
+import timeline from 'highcharts/modules/timeline';
+
+HighchartsExporting(Highcharts);
+timeline(Highcharts);
 
 interface ChartDataProps {
     series: SeriesOptionsType[] | unknown[] | any[];
@@ -16,52 +21,34 @@ const BarChart: any = () => {
     const [chartData, setChartData] = useState<ChartDataProps | null | any>(null);
 
     useEffect(() => {
-        //    const data = LoadflowCollection.getAllStations().then((result: any) => {
-        //          result.object.map((item: any) => [
-        //             item,
-        //             item.primary_substation,
-        //             item.period,
-        //             item.n_timestamps,
-        //             item.n_successful,
-        //             item.n_failed,
-        //             item.success_percentage,
-        //             item.simulation_id,
-        //             item.version_id,
-        //             item.name,
-        //         ])
-
-        //     })
-
-        const data = LoadflowCollection.getAllStations().then((result: any) => {
-
+        LoadflowCollection.getAllStations().then((result: any) => {
             setChartData({
                 series: [
                     {
-                        name: 'Loadflow',
-                        data: result.object.map((item: any) => [item.n_timestamps, item.success_percentage]),
-                        // data: data?.map((item: any) => [item[2], item[1]]),
-                        pointPlacement: 'between',
+                        name: 'Success Percentage',
+                        data: result.object.map((item: any) => [Date.parse(item.period), item.success_percentage]),
                     },
                 ],
                 title: "loadflow",
                 yAxisTitle: "Percentage",
-                xAxisTitle: "Year",
+                // xAxisTitle: "Success Percentage",
                 group: "nkforsyning",
             });
 
-        }) as any
+        })
     }, [])
 
 
     const options = {
         chart: {
-            zoomType: 'x',
             type: 'column',
+            zoomType: 'x'
         },
         title: {
             text: chartData?.title,
         },
         xAxis: {
+            type: 'datetime',
             title: {
                 text: chartData?.xAxisTitle,
             },
@@ -73,6 +60,58 @@ const BarChart: any = () => {
         },
         series: chartData?.series,
     }
+
+    // var dates = ["2019-06-14 17:00:00", "2019-06-14 17:01:00", "2021-04-13 06:15:00"]
+
+    // const options = {
+    //     chart: {
+    //         type: 'column',
+    //         zoomType: 'x'
+    //     },
+
+    //     title: {
+    //         text: 'Wavy Date Filter'
+    //     },
+
+    //     xAxis: {
+    //         type: 'datetime',
+    //         // This is from the Highcharts Stock - Stock license required
+    //         ordinal: true,
+    //         tickPositioner: function () {
+    //             return dates.map(function (date) {
+    //                 return Date.parse(date);
+    //             });
+    //         }
+    //     },
+
+    //     yAxis: {
+    //         min: 0,
+    //         title: {
+    //             text: 'Observations'
+    //         }
+    //     },
+
+    //     plotOptions: {
+    //         column: {
+    //             pointPadding: 0.2,
+    //             borderWidth: 0
+    //         }
+    //     },
+
+    //     legend: {
+    //         title: {
+    //             text: 'Observations'
+    //         }
+    //     },
+    //     series: [{
+    //         type: 'column',
+    //         data: (function () {
+    //             return dates.map(function (date, i) {
+    //                 return [Date.parse(date)];
+    //             });
+    //         })()
+    //     }]
+    // };
 
     return (
         <HighchartsReact
