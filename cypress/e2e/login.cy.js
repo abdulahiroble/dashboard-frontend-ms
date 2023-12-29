17dc811158f0f30a68efa9ef0a7aee08b4c77e0c
abdulahi roble <abdulahi_2610@hotmail.com>
2023-12-29 17:26:09 +0100

    Setup login test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,3 @@
-/// <reference types="cypress" /> 
-/// <reference types="@testing-library/cypress" />
-
 Cypress.on('uncaught:exception', (err, runnable) => {
     return false;
 });
@@ -11,26 +8,21 @@ describe("Sign in user and logout", () => {
         // Visit the app root
         cy.visit("http://localhost:3000/login");
         cy.wait(2000);
-        // navigate to login page
-        // cy.getByRole("link", { email: /login/i }).click();
-        // cy.getByRole('textbox', {
-        //     name: /email/i
-        // })
 
-        cy.findByRole('textbox', { name: /email/i })
+        // fill in the form
+        cy.findByRole("textbox", { name: /email/i }).type("aroble@gmail.com");
+        cy.findByLabelText(/password/i).type("12345");
 
-        // // fill in the form
-        // cy.findByRole("textbox", { name: /email/i }).type("aroble@gmail.com");
-        // cy.findByLabelText(/password/i).type("12345");
+        // submit the form
+        cy.findByRole('button', { name: /log in/i }).click();
 
-        // // submit the form
-        // cy.findByRole('button', { name: /log in/i }).click();
+        // wait for the page to load
+        cy.wait(5000);
 
-        // // wait for the page to load
-        // cy.wait(5000);
-
-        // // logout
-        // cy.findByText(/logout/i).click()
+        // logout
+        cy.findByText(/menu/i).click()
+        cy.wait(2000);
+        cy.findByText(/logout/i).click()
 
     });
 });
